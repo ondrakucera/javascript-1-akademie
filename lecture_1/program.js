f0403fe6920db064c3cc811709ddede23518ea16
Ondra Kučera <ondrej.kucera@centrum.cz>
2025-03-04 05:41:13 +0100

    Add result of lecture 1

---
+++ lecture_1/program.js
@@ -0,0 +1,73 @@
+// // Pozdravení uživatele
+// let vstup = prompt("Zadej svoje jméno:");
+// console.log("Ahoj, tvoje jméno je " + vstup + ".");
+
+// --------------------------------------------------
+
+// // Tvorba řetězců prokládaných daty
+// let scitanec1 = Number(prompt("Zadej první číslo:"));
+// let scitanec2 = Number(prompt("Zadej druhé číslo:"));
+// // Při spojování řetězců musíme být velmi opatrní z hlediska toho, kde začínají a kde končí které uvozovky a kde má být
+// // operátor plus.
+// console.log("Součet čísel " + scitanec1 + " a " + scitanec2 + " je " + (scitanec1 + scitanec2) + ".");
+// // Stejného výsledku můžeme alternativně dosáhnout s použitím řetězcové šablony.
+// console.log(`Součet čísel ${scitanec1} a ${scitanec2} je ${scitanec1 + scitanec2}.`);
+
+// --------------------------------------------------
+
+// // Načtení čísla od uživatele
+// let hodina = Number(prompt("Zadej, kolik je hodin:"));
+// if (hodina < 9) {
+// 	console.log("Je ráno.");
+// } else if (hodina < 12) {
+// 	console.log("Je dopoledne.");
+// } else if (hodina === 12) {
+// 	console.log("Je poledne.");
+// } else {
+// 	console.log("Je odpoledne.");
+// }
+
+// --------------------------------------------------
+
+// // Vypsání všech lichých čísel od 1 do 9
+// for (let i = 1; i <= 9; i++) {
+// 	if (i % 2 === 1) {
+// 		console.log(i);
+// 	}
+// }
+
+// --------------------------------------------------
+
+// // Pozdravení uživatele pomocí funkce
+
+// function pozdravUzivatele() {
+// 	let jmeno = prompt("Zadej svoje jméno:");
+// 	console.log(`Ahoj, tvoje jméno je ${jmeno}.`);
+// }
+
+// pozdravUzivatele();
+// pozdravUzivatele();
+
+// --------------------------------------------------
+
+// // Aritmetický průměr tří čísel
+// function aritmetickyPrumer(cislo1, cislo2, cislo3) {
+// 	let vysledek = (cislo1 + cislo2 + cislo3) / 3;
+// 	return vysledek;
+// }
+
+// let prumer1 = aritmetickyPrumer(2, 4, 7);
+// let prumer2 = aritmetickyPrumer(2, 3, -305);
+// console.log(prumer1);
+// console.log(prumer2);
+
+// --------------------------------------------------
+
+// // Funkce pro výpočet délky přepony na základě délek obou odvěsen pravoúhlého trojúhelníku
+// function delkaPrepony(odvesnaA, odvesnaB) {
+// 	let vysledek = Math.sqrt(odvesnaA * odvesnaA + odvesnaB * odvesnaB);
+// 	return vysledek;
+// }
+
+// let prepona = delkaPrepony(3, 4);
+// console.log(prepona);
